5ba6020909fb86ac0dc03b25f8e9305c717df164
DaviBorges01 <davilemosborgesbr@gmail.com>
2025-03-06 18:42:20 +0000

    Update Justeat_Bot.py

--- Justeat_Bot.py
+++ Justeat_Bot.py
@@ -32,7 +32,7 @@ def get_user_credentials(user_id):
     conn.close()  # Fecha a conexão corretamente
     return user_data if user_data else None
 
-# Função para logar na Just Eat
+# Função para logar na nova URL do Just Eat
 def login_to_justeat(user_id):
     user_data = get_user_credentials(user_id)
     if not user_data:
@@ -40,15 +40,16 @@ def login_to_justeat(user_id):
 
     email, password = user_data
 
-    driver.get("https://couriers.just-eat.co.uk/app/login")
-    time.sleep(3)  # Espera carregar
+    driver.get("https://api-produk.skipthedishes.com/auth/realms/Courier/protocol/openid-connect/auth?client_id=courier_portal_jet_uk&redirect_uri=https%3A%2F%2Fcouriers.produk.skipthedishes.com%2Fapp%2F&response_type=code&scope=openid&state=7376c76d9039482daac8d09c2d37f095&code_challenge=xT8q5468VRqoUn8u_6WvdI6w5hVha_9iszdWSAPS0FM&code_challenge_method=S256")
+    
+    time.sleep(5)  # Espera carregar a página
 
     try:
-        # Encontrar campo de login usando o novo ID "username"
-        email_field = WebDriverWait(driver, 10).until(
+        # Encontrar campo de login
+        email_field = WebDriverWait(driver, 15).until(
             EC.presence_of_element_located((By.ID, "username"))
         )
-        password_field = WebDriverWait(driver, 10).until(
+        password_field = WebDriverWait(driver, 15).until(
             EC.presence_of_element_located((By.ID, "password"))
         )
 
@@ -59,7 +60,7 @@ def login_to_justeat(user_id):
         time.sleep(5)  # Espera login
 
         # Verificar se o login foi bem-sucedido
-        if "dashboard" in driver.current_url:
+        if "dashboard" in driver.current_url or "app" in driver.current_url:
             return {"status": "Login bem-sucedido!"}
         else:
             return {"status": "Falha no login"}
